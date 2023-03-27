53ee4dfd62bc739f80e7cc0823b1c255b144d9cc
dollyp98 <114004693+dollyp98@users.noreply.github.com>
2023-03-27 11:49:59 +0200

    add project

---
+++ NewRecipe/NewRecipesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewRecipesViewController.swift
+//  NewRecipe
+//
+//  Created by Dolores Pastore on 27/03/23.
+//
+
+import UIKit
+
+class NewRecipesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
